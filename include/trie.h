6d6ab074f79fe1700ca487d486e14e5b01524fab
Zoneur <burg.lois@gmail.com>
2013-01-29 18:24:15 +0100

    Handle vowels and uppercase

--- include/trie.h
+++ include/trie.h
@@ -11,10 +11,16 @@ struct  s_trie
     trie_t  *sibling;
 };
 
+/* Allocation */
 trie_t  *trie_new_root(void);
 trie_t  *trie_new_node(const char key);
+
+/* Manipulation */
 bool  trie_add_string(const char *string, trie_t *trie);
-bool  is_in_trie(const char *string, const trie_t *trie);
 void  delete_trie(trie_t *trie);
+
+/* Lookup */
+char  *find_correction(const char *word, const trie_t *trie);
+bool  is_in_trie(const char *string, const trie_t *trie);
 
 #endif /* !__TRIE_H__ */
--- sources/trie.c
+++ sources/trie.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include "trie.h"
 
+/* Allocation */
 trie_t  *trie_new_root(void)
 {
     return trie_new_node('\0');
@@ -22,6 +24,7 @@ trie_t  *trie_new_node(const char key)
     return node;
 }
 
+/* Manipulation */
 bool  trie_add_string(const char *string, trie_t *trie)
 {
     trie_t  *child = trie->child;
@@ -68,6 +71,20 @@ bool  trie_add_string(const char *string, trie_t *trie)
     return true;
 }
 
+void  delete_trie(trie_t *trie)
+{
+    trie_t  *tmp = NULL;
+
+    while (trie != NULL)
+    {
+        delete_trie(trie->child);
+        tmp = trie;
+        trie = trie->sibling;
+        free(tmp);
+    }
+}
+
+/* Lookup */
 bool  is_in_trie(const char *string, const trie_t *trie)
 {
     const trie_t  *child = trie->child;
@@ -89,15 +106,102 @@ bool  is_in_trie(const char *string, const trie_t *trie)
     return false;
 }
 
-void  delete_trie(trie_t *trie)
+static bool case_char_cmp(char c1, char c2)
 {
-    trie_t  *tmp = NULL;
+    return strncasecmp(&c1, &c2, 1) == 0;
+}
 
-    while (trie != NULL)
+static bool vowel_char_cmp(char c1, char c2)
+{
+    /*
+    ** Puts c1 and c2 in lower case.
+    */
+    if (c1 >= 'A' && c1 <= 'Z')
+        c1 += ('a' - 'A');
+    if (c2 >= 'A' && c2 <= 'Z')
+        c2 += ('a' - 'A');
+
+    if ((c1 == 'a' || c1 == 'e' || c1 == 'i' || c1 == 'o' || c1 == 'u' || c1 == 'y') &&
+        (c2 == 'a' || c2 == 'e' || c2 == 'i' || c2 == 'o' || c2 == 'u' || c2 == 'y'))
+        return true;
+    return false;
+}
+
+/*
+** Kind of nasty.. I don't like repeating the loop but I
+** also don't like the idea of a sub-function recursively
+** calling its caller...
+*/
+static char *rec_find_correction(const char *word, const size_t corrected_word_size,
+                                 const trie_t *trie)
+{
+    const trie_t  *child = trie->child;
+
+    /*
+    ** First loop to see if we got the word as is.
+    ** We don't want 'hell' to be corrected as 'Hell'
+    */
+    while (child != NULL)
     {
-        delete_trie(trie->child);
-        tmp = trie;
-        trie = trie->sibling;
-        free(tmp);
+        if (child->key == word[0])
+        {
+            char  *corrected_word = NULL;
+            if (word[0] == '\0')
+            {
+                //we found the whole word, so let's allocate some space to store it
+                corrected_word = malloc((corrected_word_size + 1) *
+                                        sizeof(*corrected_word));
+            }
+            else
+            {
+                //we didn't find the word yet, keep looking
+                corrected_word = rec_find_correction(word + 1,
+                                                     corrected_word_size + 1,
+                                                     child);
+            }
+            //if the word was found/allocated..
+            if (corrected_word != NULL)
+            {
+                corrected_word[corrected_word_size] = child->key;//..we build it
+                return corrected_word;//the word is built as we go back the call stack
+            }
+        }
+        else if (child->key > word[0])
+            break;//impossible to find the word anymore without spelling correction
+        child = child->sibling;
     }
+
+    /* Second loop, applying suggestion here */
+    child = trie->child;
+    while (child != NULL)
+    {
+        if (case_char_cmp(child->key, word[0]) == true ||
+            vowel_char_cmp(child->key, word[0]) == true)
+        {
+            char  *corrected_word = NULL;
+            if (word[0] == '\0')
+            {
+                corrected_word = malloc((corrected_word_size + 1) *
+                                        sizeof(*corrected_word));
+            }
+            else
+            {
+                corrected_word = rec_find_correction(word + 1,
+                                                     corrected_word_size + 1,
+                                                     child);
+            }
+            if (corrected_word != NULL)
+            {
+                corrected_word[corrected_word_size] = child->key;//..we build it
+                return corrected_word;//the word is built as we go back the call stack
+            }
+        }
+        child = child->sibling;
+    }
+    return NULL;
+}
+
+char  *find_correction(const char *word, const trie_t *trie)
+{
+    return rec_find_correction(word, 0, trie);
 }
